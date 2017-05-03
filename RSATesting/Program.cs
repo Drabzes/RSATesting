72bba2b8b49cf2c095f06a475bb9469132834446
Drabzes <reynders_giel@hotmail.com>
2017-05-03 09:22:59 +0200

    Complete RSA

---
+++ RSATesting/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSATesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lets take a new CSP with a new 2048 bit rsa key pair
+            var csp = new RSACryptoServiceProvider(2048);
+
+            //how to get the private key
+            var privKey = csp.ExportParameters(true);
+
+            //and the public key ...
+            var pubKey = csp.ExportParameters(false);
+
+            //converting the public key into a string representation
+            string pubKeyString;
+            {
+                //we need some buffer
+                var sw = new System.IO.StringWriter();
+                //we need a serializer
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                //serialize the key into the stream
+                xs.Serialize(sw, pubKey);
+                //get the string from the stream
+                pubKeyString = sw.ToString();
+            }
+
+            string privKeyString;
+            {
+                //we need some buffer
+                var sw = new System.IO.StringWriter();
+                //we need a serializer
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                //serialize the key into the stream
+                xs.Serialize(sw, privKey);
+                //get the string from the stream
+                privKeyString = sw.ToString();
+            }
+            string nameFile = string.Format("publicKeyGiel.dat");
+
+            saveByte(pubKeyString, nameFile);
+            pubKeyString = getString(nameFile);
+
+            nameFile = string.Format("privateKeyGiel.dat");
+
+            saveByte(privKeyString, nameFile);
+            privKeyString = getString(nameFile);
+
+            //converting it back
+            {
+                //get a stream from the string
+                var sr = new System.IO.StringReader(pubKeyString);
+                //we need a deserializer
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                //get the object back from the stream
+                pubKey = (RSAParameters)xs.Deserialize(sr);
+            }
+            
+            //converting it back
+            {
+                //get a stream from the string
+                var sr = new System.IO.StringReader(privKeyString);
+                //we need a deserializer
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                //get the object back from the stream
+                privKey = (RSAParameters)xs.Deserialize(sr);
+            }
+
+            //conversion for the private key is no black magic either ... omitted
+
+            //we have a public key ... let's get a new csp and load that key
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(pubKey);
+
+            //we need some data to encrypt
+            var testData = "foobar";
+
+            var md5hash = CalculateMD5Hash(testData);
+            Console.WriteLine(string.Format("Oorspronkelijke MD5 hash: {0}", md5hash));
+
+            saveByte(testData, nameFile);
+
+            var plainTextData = getString(nameFile);
+
+
+            //for encryption, always handle bytes...
+            var bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(plainTextData);
+
+            //apply pkcs#1.5 padding and encrypt our data 
+            var bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+
+            //we might want a string representation of our cypher text... base64 will do
+            var cypherTextForFile = Convert.ToBase64String(bytesCypherText);
+
+            
+            nameFile = string.Format("encryptText.dat");
+
+            saveByte(cypherTextForFile, nameFile);
+
+            var cypherText = getString(nameFile);
+
+            /*
+             * some transmission / storage / retrieval
+             * 
+             * and we want to decrypt our cypherText
+             */
+
+            //first, get our bytes back from the base64 string ...
+            bytesCypherText = Convert.FromBase64String(cypherText);
+
+
+            //we want to decrypt, therefore we need a csp and load our private key
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(privKey);
+
+            //decrypt and strip pkcs#1.5 padding
+            bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+
+            //get our original plainText back...
+            plainTextData = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+            md5hash = CalculateMD5Hash(plainTextData);
+            
+
+            Console.WriteLine(string.Format("Data decrypted: {0}", plainTextData));
+
+            Console.WriteLine("");
+            Console.WriteLine(string.Format("Na dycryptie MD5 hash: {0}", md5hash));
+            Console.ReadKey();
+        
+        }
+
+        private static void saveByte(string str, string nameFile)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            File.WriteAllBytes(nameFile, bytes);
+        }
+
+        private static string getString( string nameFile)
+        {
+            byte[] bytes = File.ReadAllBytes(nameFile);
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        public static string CalculateMD5Hash(string input)
+
+        {
+
+            // step 1, calculate MD5 hash from input
+
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+
+            // step 2, convert byte array to hex string
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+
+            {
+
+                sb.Append(hash[i].ToString("X2"));
+
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
